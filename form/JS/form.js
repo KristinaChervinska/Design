cfab95eaeff3eceb51eaff43d0f0315896308d78
olleg12 <olleg12@ukr.net>
2018-02-20 21:18:53 +0200

    add .

---
+++ form/JS/form.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+	var result=document.getElementById('userinput').value;
+	     alert('result');
+};
+
+function myFunction() {
+	if Age = Number(prompt('Please enter your age', 0));
+   if (age < 16) {
+	   alert('You are underage!')
+	   } else {
+		   alert('You are adult!')
+		   };
+};
+
+
+
+
+
